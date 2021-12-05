df68a2d31bf8a82174391255a057084ffe5c680a
mdmegha1999 <94183187+mdmegha1999@users.noreply.github.com>
2021-12-05 21:06:20 +0530

    tanu

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,21 @@
+// import using require
+
 // declare class
 
+
 // export class using module.exports
+
+let Shape = require('./shape')
+
+
+export class Circle extends Shape {
+    constructor(color) {
+        super(color);
+    }
+    calculateArea() {
+        console.log(`${this.color}  area is calculated`)
+
+    }
+}
+
+module.exports = Circle
